e4e70feff58124d3f2a2bc0c4fafc7b630501a08
Kevin Recuerda <kevin.recuerda@gmail.com>
2021-11-27 15:09:16 +0100

    use baseurl & path

--- sample/recwhale-ts/moment-helper.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var recwhale_ts_1 = require("recwhale-ts");
-console.log(recwhale_ts_1.MomentHelper.test());
--- sample/recwhale-ts/moment-helper.ts
+++ sample/recwhale-ts/moment-helper.ts
@@ -1,3 +1,8 @@
 ﻿import {MomentHelper} from "recwhale-ts";
+import "recwhale-ts";
 
-console.log(MomentHelper.test())
+console.log(MomentHelper.test());
+
+const e = ["a", "b", "c"];
+const f = e.last();
+console.log(f);
