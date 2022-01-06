4641d042810fa09a3592809112b5b1d24e3119a9
Kevin Recuerda <kevin.recuerda@gmail.com>
2022-01-06 11:00:40 +0100

    config

--- src/recwhale-react-bootstrap/vite.config.ts
+++ src/recwhale-react-bootstrap/vite.config.ts
@@ -1,19 +1,27 @@
 ﻿// vite.config.ts
 import {defineConfig} from "vite";
 import path           from "path";
-import react          from "@vitejs/plugin-react";
+// import react          from "@vitejs/plugin-react";
 // import dts            from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-                                plugins: [react()],
+                                // plugins: [react()],
                                 build: {
                                     lib: {
                                         entry:    path.resolve(__dirname, "src/index.ts"),
                                         name:     "recwhale-react-bootstrap",
                                         fileName: "index"
+                                    },
+                                    rollupOptions: {
+                                        external: ["react"],
+                                        output: {
+                                            globals: {
+                                                react: "react"
+                                            }
+                                        }
                                     }
                                 }
-                                });
+                            });
 
 // import tsconfigPaths from 'vite-tsconfig-paths' // can remove if you don't use ts config paths
 // import reactRefresh from '@vitejs/plugin-react-refresh'
