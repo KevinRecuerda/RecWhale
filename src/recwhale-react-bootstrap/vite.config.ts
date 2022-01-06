cc1ec7e1bf7efdf31689d6e6d3804ca9a8e172cb
Kevin Recuerda <kevin.recuerda@gmail.com>
2022-01-06 13:44:40 +0100

    use dts

--- src/recwhale-react-bootstrap/vite.config.ts
+++ src/recwhale-react-bootstrap/vite.config.ts
@@ -2,19 +2,19 @@
 import {defineConfig} from "vite";
 import path           from "path";
 // import react          from "@vitejs/plugin-react";
-// import dts            from "vite-plugin-dts";
+import dts            from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-                                // plugins: [react()],
-                                build: {
-                                    lib: {
+                                plugins: [dts()],
+                                build:   {
+                                    lib:           {
                                         entry:    path.resolve(__dirname, "src/index.ts"),
                                         name:     "recwhale-react-bootstrap",
                                         fileName: "index"
                                     },
                                     rollupOptions: {
                                         external: ["react"],
-                                        output: {
+                                        output:   {
                                             globals: {
                                                 react: "react"
                                             }
