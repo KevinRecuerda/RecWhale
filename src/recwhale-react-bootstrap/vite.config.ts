563123deb0a753f08c9207df3489814b08721347
Kevin Recuerda <kevin.recuerda@gmail.com>
2022-01-08 16:47:50 +0100

    test

--- src/recwhale-react-bootstrap/vite.config.ts
+++ src/recwhale-react-bootstrap/vite.config.ts
@@ -2,15 +2,17 @@
 import {defineConfig} from "vite";
 import path           from "path";
 // import react          from "@vitejs/plugin-react";
+import dts            from "vite-dts";
 // import dts            from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-                                // plugins: [dts()],
+                                plugins: [dts()],
                                 build:   {
                                     lib:           {
                                         entry:    path.resolve(__dirname, "src/index.ts"),
                                         name:     "recwhale-react-bootstrap",
-                                        fileName: "index"
+                                        fileName: "index",
+                                        formats: ['es']
                                     },
                                     rollupOptions: {
                                         external: ["react"],
