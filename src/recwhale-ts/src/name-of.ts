f96f78773bd274889f4af4577969a2e7245543d4
Kevin Recuerda <kevin.recuerda@gmail.com>
2022-01-25 17:59:59 +0100

    Update name-of.ts

--- src/recwhale-ts/src/name-of.ts
+++ src/recwhale-ts/src/name-of.ts
@@ -1,4 +1,4 @@
-function nameof2<T>(selector: (item: T) => any, skipPath?: number): string {
+export function nameof<T>(selector: (item: T) => any, skipPath?: number): string {
   skipPath ??= 0;
   const paths = selector.toString().split(".").slice(1 + skipPath);
   const name = paths.join(".");
