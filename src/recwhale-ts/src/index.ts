ebfe9bc31994ad18d456a2e8cb7752e72bb01a89
Kevin Recuerda <kevin.recuerda@gmail.com>
2022-01-25 14:04:26 +0100

    nameof

--- src/recwhale-ts/src/index.ts
+++ src/recwhale-ts/src/index.ts
@@ -2,6 +2,7 @@
 export * from "./enum-helper";
 export * from "./map-extensions";
 export * from "./moment-extensions";
+export * from "./name-of";
 export * from "./number-extensions";
 export * from "./promise-helper";
 export * from "./string-extensions";
---
+++ src/recwhale-ts/src/name-of.ts
@@ -0,0 +1,6 @@
+﻿export function nameof<T>(item: T, key: keyof T): string;
+export function nameof<T>(key: keyof T): string;
+
+export function nameof(key1: any, key2?: any): any {
+    return key2 ?? key1;
+}
