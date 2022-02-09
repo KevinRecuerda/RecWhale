1841eff3712f87c40274fadb19a2acdec13664f9
Kevin Recuerda <kevin.recuerda@gmail.com>
2022-02-09 15:37:50 +0100

    export builtin

--- src/recwhale-react-ag-grid/src/index.ts
+++ src/recwhale-react-ag-grid/src/index.ts
@@ -1,5 +1,7 @@
 export * from "ag-grid-react";
 export * from "./Ag";
 export * from "./AgCrud";
+export * from "./BuiltIn";
+export * from "./BuiltInExtended";
 export * from "./columns/AgCol";
 export * from "./context";
